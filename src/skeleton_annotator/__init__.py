22e4c2dffac40ee3c39c7f12e9322d1dd0ba0690
RyuuYou <ryuuyou0529@gmail.com>
2024-07-27 17:26:56 +0800

    initial commit

---
+++ src/skeleton_annotator/__init__.py
@@ -0,0 +1,19 @@
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from ._writer import write_multiple, write_single_image
+
+__all__ = (
+    "napari_get_reader",
+    "write_single_image",
+    "write_multiple",
+    "ExampleQWidget",
+    "ImageThreshold",
+    "threshold_autogenerate_widget",
+    "threshold_magic_widget",
+)
